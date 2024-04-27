b976f6f93364a003688a437ce6b609cc9ce27e3c
morphin93 <aqwijnrfv@gmail.com>
2024-04-27 15:54:24 +0200

    interface ok6

--- comp/upperPart.js
+++ comp/upperPart.js
@@ -2,9 +2,7 @@
 Vue.component('upper-part', {
     template: `
         <div id="upperPart">
-            <!-- Contenu spécifique de la partie supérieure -->
-            <h1>Welcome to the Upper Part</h1>
-            <!-- Vous pouvez ajouter plus de contenu ici selon vos besoins -->
+             
         </div>
     `
 });
