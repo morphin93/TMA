cf4184659b2968af85ad64429e1fc977bf2dd7ba
morphin93 <aqwijnrfv@gmail.com>
2024-04-27 15:20:08 +0200

    interface teste7

---
+++ comp/upperPart.js
@@ -0,0 +1,10 @@
+// upperPart.js
+Vue.component('upper-part', {
+    template: `
+        <div id="upperPart">
+            <!-- Contenu spécifique de la partie supérieure -->
+            <h1>Welcome to the Upper Part</h1>
+            <!-- Vous pouvez ajouter plus de contenu ici selon vos besoins -->
+        </div>
+    `
+});
