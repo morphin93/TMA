c25ba4e84fa5fc54f797a89281976c5123258ea9
morphin93 <aqwijnrfv@gmail.com>
2024-04-27 15:07:38 +0200

    interface teste2

---
+++ comp/interactionMenu.js
@@ -0,0 +1,26 @@
+// interactionMenu.js
+Vue.component('interaction-menu', {
+    template: `
+        <div id="interactionMenu">
+            <button @click="fetchData">Teste</button>
+            <p>{{ coinData }}</p>
+        </div>
+    `,
+    data() {
+        return {
+            coinData: ''
+        };
+    },
+    methods: {
+        fetchData() {
+            axios.get('url')
+                .then(response => {
+                    this.coinData = response.data[0];
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                    Telegram.WebApp.sendData('Error fetching data');
+                });
+        }
+    }
+});
