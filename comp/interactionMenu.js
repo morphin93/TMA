515e676af06066071ea8658d128060a0527cc9e9
morphin93 <aqwijnrfv@gmail.com>
2024-04-28 09:45:03 +0200

    wallet integration4

--- comp/interactionMenu.js
+++ comp/interactionMenu.js
@@ -2,8 +2,7 @@
 Vue.component('interaction-menu', {
     template: `
         <div id="interactionMenu">
-            <button @click="fetchData">Teste2</button>
-            <p>{{ coinData }}</p>
+            
            
         </div>
     `,
@@ -13,15 +12,6 @@ Vue.component('interaction-menu', {
         };
     },
     methods: {
-        fetchData() {
-            axios.get('url')
-                .then(response => {
-                    this.coinData = response.data[0];
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                    Telegram.WebApp.sendData('Error fetching data');
-                });
-        }
+ 
     }
 });
